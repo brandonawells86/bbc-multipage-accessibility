e6b611c2d3c7f235f8170f2cc836fe260d3dd44d
Brandon Wells <brandon.awells.github@gmail.com>
2021-10-21 00:26:45 -0400

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,33 @@
+function fontSize(change, factor) {
+    var f = parseInt(factor);
+    var body = document.body; 
+    style = window.getComputedStyle(body, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    
+    if (change=='inc'){
+        body.style.fontSize = (currentSize + f) + 'px';
+    }else if (change=='dec'){
+        body.style.fontSize = (currentSize - f) + 'px';
+    }
+
+    // switch (change) {
+    //     case 'inc':
+    //         body.style.fontSize = (currentSize + f) + 'px';
+    //         break;
+    //     case 'dec':
+    //         body.style.fontSize = (currentSize - f) + 'px';
+    //         break;
+    //     }
+}
+
+function setTheme(theme) {
+    if (theme=='light'){
+        document.getElementById('theme-id').href= './css/light.css'
+    }else if (theme=='dark') {
+        document.getElementById('theme-id').href= './css/dark.css'
+    }else if (theme=='neon') {
+        document.getElementById('theme-id').href= './css/neon.css'
+    }else {
+        document.getElementById('theme-id').href= './css/styles.css'
+    }
+}
